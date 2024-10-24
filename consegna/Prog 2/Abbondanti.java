3d2e6b479275b3e257ddd1abb2416984edfd1d6b
Emanuele Moro <146356995+hikerema@users.noreply.github.com>
2024-10-24 18:24:32 +0200

    Lab1

--- consegna/Prog 2/Abbondanti.java
+++
@@ -1,9 +0,0 @@
-
-public class Abbondanti {
-//OVERVIEW: 
-    public static List<Integer> elencoDivisoriPropri(int n) {
-    //EFFECTS: restituisce una list di int contenente tutti i divisori propri di n
-        
-    }
-    
-}
--- consegna/Prog 2/TimeBusyException.java
+++
@@ -1,9 +0,0 @@
-public class TimeBusyException extends RuntimeException {
-
-    public TimeBusyException() {
-        super();
-    }
-    public TimeBusyException(String messaggio) {
-        super(messaggio);
-    }
-}
