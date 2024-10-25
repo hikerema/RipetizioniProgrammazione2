1658d1ccb3e4131311e914d2e754687e7e34988a
Emanuele Moro <146356995+hikerema@users.noreply.github.com>
2024-10-25 11:06:48 +0200

    defualt

---
+++ compiti/consegna_automatica/cosegna_compiti.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d ".\RipetizioniProgrammazione2"
+git add .
+git commit -m "Cosegna compiti automatica"
+git push origin main
+pause
---
+++ compiti/consegna_automatica/prima_di_lavorare.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d ".\RipetizioniProgrammazione2"
+git pull
+pause
